4fa39db52292ac50a28afad7a9947205b675b824
Mineaworld <mineawebdev@gmail.com>
2024-11-22 14:08:32 +0700

    Updated import

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./Weather.css";
 import SearchIcon from "../assets/search.png";
 import ClearIcon from "../assets/clear.png";
